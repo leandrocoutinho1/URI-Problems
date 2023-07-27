052fd9ac61e396b11640baa50d727ab76cf9074c
L-coutinho <leandrocoutinho.tucano@gmail.com>
2023-07-27 19:24:10 -0300

    Alterações Arquivo 1098

--- URI_1098.py
+++ URI_1098.py
@@ -1,10 +1,20 @@
 i = 0
+cont = 1
 while i <= 2:
-    for j in range(1, 4):
-        if (i==0) or (i==1.0) or (i==2.0):
-            i = int(i)
-            j = int(j)
-            print(f"I={i} J={i+j}")
-        else:
+    if (cont==1):
+        print(f"I=0 J=1")
+        print(f"I=0 J=2")
+        print(f"I=0 J=3")
+    elif (cont==6):
+        print(f"I=1 J=2")
+        print(f"I=1 J=3")
+        print(f"I=1 J=4")
+    elif (i>1.9 and i<2.1):
+        print(f"I=2 J=3")
+        print(f"I=2 J=4")
+        print(f"I=2 J=5")
+    else:
+        for j in range(1, 4):
             print(f"I={round(i, 2)} J={round(i+j, 2)}")
     i += 0.2
+    cont += 1
